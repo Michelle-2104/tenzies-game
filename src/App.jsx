16a7e81161c3c9c388b1bb91ef85b3f7d55c474d
Michelle2104 <chidowemichelle@gmail.com>
2023-01-25 18:52:11 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Die from './Die'
 
 export default function App() {
-  return (
+     
+      '['
+      return (
     <main>
-      <div>Hello </div>
-
+           <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
     </main>
   )
 }
